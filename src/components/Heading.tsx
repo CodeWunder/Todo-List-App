76b62b90bccd604b51dba8f028b6df6625e3a069
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2023-08-14 09:44:10 +0100

    Update Heading.tsx

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,6 @@
 const Heading = () => {
   return (
-    <h1 className='py-3 text-center text-4xl font-bold text-gray-900'>
+    <h1 className='py-3 text-center text-3xl font-bold text-gray-900'>
       <span className='bg-gradient-to-r from-purple-500 to-pink-600 bg-clip-text text-transparent'>
         CodeWunder
       </span>
